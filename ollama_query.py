ebfd49c8af7d6dc0d9f1d787e3c385540b9c28c4
Corentin Guichaoua <corentin.guichaoua@gmail.com>
2024-12-06 09:37:00 +0100

    Extracted ollama query to its own module

---
+++ ollama_query.py
@@ -0,0 +1,34 @@
+
+import requests
+import json
+
+def ollama_query(prompt, model="llama3.2", api_url="http://localhost:11434/api/generate"):
+    """
+    Interroge Ollama via une API REST locale.
+    
+    Parameters:
+    - prompt: La question ou la commande à exécuter.
+    - model: Le modèle Ollama à utiliser (par défaut : "llama3.2").
+    - api_url: L'URL de l'API Ollama (par défaut : "http://localhost:11434/api/generate").
+    
+    Returns:
+    - La réponse générée par Ollama.
+    """
+    headers = {
+        "Content-Type": "application/json"
+    }
+    data = {
+        "model": model,
+        "prompt": prompt
+    }
+    try:
+        response = requests.post(api_url, json=data, headers=headers)
+        response.raise_for_status()  # Lève une exception si le statut HTTP est une erreur
+        text = response.text.strip()
+        lines = text.split("\n")
+        tokens = list(map(lambda line: json.loads(line)["response"], lines))
+        formated = "".join(tokens)
+        answer = formated.strip()
+        return answer
+    except requests.exceptions.RequestException as e:
+        raise RuntimeError(f"Erreur lors de la requête à Ollama : {e}")
--- rag_pipeline.py
+++ rag_pipeline.py
@@ -7,44 +7,13 @@ from langchain_community.vectorstores import FAISS
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.prompts import PromptTemplate
 import numpy as np
-import requests
 from extract_pdf import extract_content_from_pdf
-import json
 import os
 import io
 
-# Fonction pour interroger Ollama
+from ollama_query import ollama_query # Fonction pour interroger Ollama
 
-def ollama_query(prompt, model="llama3.2", api_url="http://localhost:11434/api/generate"):
-    """
-    Interroge Ollama via une API REST locale.
-    
-    Parameters:
-    - prompt: La question ou la commande à exécuter.
-    - model: Le modèle Ollama à utiliser (par défaut : "llama3.2").
-    - api_url: L'URL de l'API Ollama (par défaut : "http://localhost:11434/api/generate").
-    
-    Returns:
-    - La réponse générée par Ollama.
-    """
-    headers = {
-        "Content-Type": "application/json"
-    }
-    data = {
-        "model": model,
-        "prompt": prompt
-    }
-    try:
-        response = requests.post(api_url, json=data, headers=headers)
-        response.raise_for_status()  # Lève une exception si le statut HTTP est une erreur
-        text = response.text.strip()
-        lines = text.split("\n")
-        tokens = list(map(lambda line: json.loads(line)["response"], lines))
-        formated = "".join(tokens)
-        answer = formated.strip()
-        return answer
-    except requests.exceptions.RequestException as e:
-        raise RuntimeError(f"Erreur lors de la requête à Ollama : {e}")
+
 
 # Définir le contexte initial
 DEFAULT_CONTEXT = """
